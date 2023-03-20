0566725da0d45610a2630fb051ae1e64038f8fd0
Patelyash2010 <104272540+Patelyash2010@users.noreply.github.com>
2023-03-20 17:55:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Quote from "./Quote";
+import Title from "./Title";
+
+
+const App = () => {
+ return(
+   <>
+  <Title/>
+  
+  <Quote/>  
+   </>
+    )
+};
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <nav className="navbar navbar-dark  ">
+            <span className="navbar-brand mb-0 h1 mx-auto">Quote of the day</span>
+    </nav>
+  )
+}
+
+export default Title
